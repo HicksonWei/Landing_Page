16b5b2f66fec63413e2d4113c858f2adf8a34fe8
HicksonWei <hanks199021@gmail.com>
2017-12-19 12:59:56 +0800

    Update 2017-12-19T04:59:49.512Z

---
+++ js/all.js
@@ -0,0 +1,2 @@
+"use strict";window.addEventListener("load",function(){var t=document.getElementById("needs-validation");t.addEventListener("submit",function(e){!1===t.checkValidity()&&(e.preventDefault(),e.stopPropagation()),t.classList.add("was-validated")},!1)},!1);
+//# sourceMappingURL=all.js.map
